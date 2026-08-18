c107956ab3bd7b517bcf6eb3f91df1461a03010d
Abel Tiago <abeltiago1@gmail.com>
2026-08-18 22:42:19 +0100

    chore: bootstrap Spring Boot 4.1 skeleton (Java 21, Spring Web)

---
+++ src/test/java/com/abeltiago/tinyledger/TinyLedgerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abeltiago.tinyledger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TinyLedgerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
